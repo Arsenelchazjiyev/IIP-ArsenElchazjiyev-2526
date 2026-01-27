9071d6ef7018cd9b4a98c2844349523bcdad1073
Arsenelchazjiyev <arsen.elchazjiyev@gmail.com>
2026-01-27 14:44:15 +0100

    .

---
+++ IIP2.Examen/ConsoleSnelheidsControle/program.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace ConsoleSnelheidsControle
+{
+    class Program
+    {
+	static void Main(string[] args)
+        {
+
+			
+			
+		}
+		
+	private static int LeesSnelheden()
+	{
+		
+	}
+	
+	private static string BerekenKmTeSnel()
+	{
+		
+	}
+	
+	private static string OvertredingenPerCategorie()
+	}
+}
